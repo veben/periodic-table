44ecb43f6f0db4c54bbfba42a6829cfb3bea2f1c
veben <benoit.veyriere@external.grtgaz.com>
2019-11-23 21:08:15 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'periodic-table-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'periodic-table';
+}
