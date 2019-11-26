175e0ad5a4ba20ddfaa8f55d061c1ec503ad13e5
veben <>
2019-11-27 00:25:06 +0100

    Created method to define css class of element. Added more global css variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,20 @@ export class AppComponent {
 
   elements: ElementModel[] = data["default"];
 
-  groupToCssClass = {
-    "Nonmetal": "nonmetal",
-    "Noble Gas": "noble-gas",
-    "Alkali Metal": "alkali-metal",
-    "Alkaline Earth Metal": "alkaline-earth-metal",
-    "Metalloid": "metalloid",
-    "Halogen": "halogen",
-    "Post-Transition Metal": "post-transition-metal",
-    "Transition Metal": "transition-metal",
-    "Lanthanide": "lanthanide",
-    "Actinide": "actinide"
+  getCssClassesForElement(element: ElementModel): string {
+    const groupToCssClass = {
+      "Nonmetal": "nonmetal",
+      "Noble Gas": "noble-gas",
+      "Alkali Metal": "alkali-metal",
+      "Alkaline Earth Metal": "alkaline-earth-metal",
+      "Metalloid": "metalloid",
+      "Halogen": "halogen",
+      "Post-Transition Metal": "post-transition-metal",
+      "Transition Metal": "transition-metal",
+      "Lanthanide": "lanthanide",
+      "Actinide": "actinide"
+    };
+
+    return "element an" + element.atomicNumber + ' ' + groupToCssClass[element.group];
   }
 }
